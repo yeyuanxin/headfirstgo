5b7981f3cd1e65b26ad5a7b43254bd286695e409
yeyuanxin <xinyy_room@126.com>
2021-02-22 13:04:26 +0800

    Add files via upload

---
+++ greeting/greeting.go
@@ -0,0 +1,16 @@
+package greeting
+
+import "fmt"
+
+func Hello(){
+	fmt.Println("Hello!")
+}
+
+func Hi(){
+	fmt.Println("Hi!")
+}
+
+func AllGreetings(){
+	Hello()
+	Hi()
+}
---
+++ hi/main.go
@@ -0,0 +1,9 @@
+package main
+
+//import (greeting "test/greeting")
+import "github.com/headfirstgo/greeting"
+
+func main() {
+	greeting.Hello()
+	greeting.Hi()
+}
